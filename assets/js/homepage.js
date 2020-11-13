de14a7041c90b90996a4b03a36506becff5466e8
JRJ <jjsocc171@msn.com>
2020-11-13 14:08:06 -0600

    fixed bugs with repos issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,28 +9,6 @@
 //      });
 //     //  console.log("outside");
 // };
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "http://api.github.com/users/" + user + "/repos";
-
-    // make a request to the url
-    fetch(apiUrl).then(function(response){
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-            // console.log(data);
-            displayRepos(data, user);
-        });
-    } else {
-        alert("Error: " + response.statusText);
-    }
-    })
-    .catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connecto to GitHub");
-    })
-};
-// getUserRepos();
 
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
@@ -45,22 +23,51 @@ var fromSubmitHandler = function(event) {
 
     if (username) {
         getUserRepos(username);
+
+        //clear old content
+        repoContainerEl.textContent = "";
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
     }
 }
 
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "http://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        // request was successful
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+                displayRepos(data, user);
+            });
+        } else {
+        alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connecto to GitHub");
+    });
+};
+// getUserRepos();
+
+
 var displayRepos = function(repos, searchTerm) {
     // console.log(repos);
     // console.log(searchTerm);
+
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
     // clear old content
-    repoContainerEl.textContent = "";
+    // repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
     //loop over repos
@@ -85,15 +92,17 @@ var displayRepos = function(repos, searchTerm) {
 
         //check of current repo has issues or not
         if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML = "i class='fas fa-times status-icon-danger;></i>" + repos[i].open_issues_count + "issue(s)";
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
-            statusEl.innerHTML = "<i class= 'fas fa-check-spuare-icon icon-success'></i>";
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
+        
+        // append to container
+        repoContainerEl.appendChild(statusEl);
 
         // append container to the DOM
         repoContainerEl.appendChild(repoEl);
     }
-}
-
-
+};
+// add event listeners to forms
 userFormEl.addEventListener("submit", fromSubmitHandler);
